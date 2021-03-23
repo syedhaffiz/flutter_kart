06ef0335b4889948e89e120868eace2b1d0569ce
Syed Haffiz <syed.haffiz@kloudlearn.com>
2021-03-23 21:17:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/sh/flutter_kart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sh.flutter_kart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Error: ${snapshot.error}"),
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: Center(
+              child: Text("Firebase initialized!"),
+            ),
+          );
+        }
+
+        return Scaffold(
+          body: Center(
+            child: Text("Initializing Firebase...."),
+          ),
+        );
+      },
+    );
+  }
+}
